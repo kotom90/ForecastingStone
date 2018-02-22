56987da1b5d41a81c5e6b1b826a7bc6ebc435b11
Kostas <>
2018-02-22 18:27:03 +0200

    Prosthiki GUI gia provlepsh kairou.

--- src/eWeather.java
+++ src/eWeather.java
@@ -1,10 +1,19 @@
 import GUI.GUIMainMenu;
+import javax.swing.JFrame;
 
 public class eWeather 
 {
+    //τίτλος βασικού παραθύρου
+    public static final String textMainFrame = "eWeather";
+    
     public static void main(String[] args)
     {
-	new GUIMainMenu();
+	JFrame mainFrame = new JFrame();
+	mainFrame.setSize(800, 500);
+	mainFrame.setTitle(textMainFrame);
+	mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	mainFrame.setVisible(true);
+	new GUIMainMenu(mainFrame);
 	//new JsonRequesturu();
     }
 }
